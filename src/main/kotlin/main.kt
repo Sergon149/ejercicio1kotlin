95d301f8d4ddda05c807ee3a0e66737ffb651764
Sergio <sergon149@alu.centroafuera.es>
2021-09-16 13:48:11 +0200

    Initial commit

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,9 @@
+fun main(){
+    var num1 = 13L
+    println("El número $num1 es mi numero favorito")
+
+    println("Sin enbargo, ${num1.toDouble()} no me gusta")
+
+    println(num1.and(7))
+    println(num1.compareTo(13))
+}
